7013159544badc3a4a157e72af92846dd56ea86f
Marcus Ulrich <mrulrich@Sotty-Jr.local>
2014-02-24 15:34:00 +0100

    Adding more tests

--- ibacklog/ibacklog_tests.py
+++ ibacklog/ibacklog_tests.py
@@ -52,6 +52,12 @@ class TestIBacklogFunctions(unittest.TestCase):
         self.story6 = story6 = Story("6", 5, 3)
         return [story1, story2, story3, story4, story5, story6]
 
+    def _getFakeStoryList2(self):
+        self.story1 = story1 = Story("1", 1, 1)
+        self.story2 = story2 = Story("2", 1, 1)
+        self.story3 = story3 = Story("3", 1, 3)
+        return [story1, story2, story3]
+
     def test_getSprintNegativeInput(self):
         # Make sure IBacklog.getSprint returns [] for negative totalPointsAchievable input.
         storylist = self._getFakeStoryList1()
@@ -76,12 +82,16 @@ class TestIBacklogFunctions(unittest.TestCase):
     def test_getSprintAllPointsTooHigh(self):
         # Make sure IBacklog.getSprint returns [] when all stories have Point value higher than
         # totalPointsAchievable input.
-        storylist = self._getFakeStoryList1()
+        story1 = Story("1", 2, 1)
+        story2 = Story("2", 3, 1)
+        story3 = Story("3", 4, 1)
+        storylist = [story1, story2, story3]
         datastore_mock = Mock()
         datastore_mock.__class__ = IDatastore
         datastore_mock.readStory.return_value = storylist
         backlog = IBacklog(datastore_mock)
-        actual_storylist = backlog.getSprint(10)
+        actual_storylist = backlog.getSprint(1)
+        self.assertEqual(actual_storylist, [])
  
     def test_getSprintNormal1(self):
         # Make sure IBacklog.getSprint returns the maximum stories for totalPointsAchievable input.
@@ -96,7 +106,25 @@ class TestIBacklogFunctions(unittest.TestCase):
         self.assertIn(self.story1, actual_storylist)
         self.assertIn(self.story3, actual_storylist)
         self.assertIn(self.story6, actual_storylist)
+        self.assertNotIn(self.story2, actual_storylist)
+        self.assertNotIn(self.story4, actual_storylist)
+        self.assertNotIn(self.story5, actual_storylist)
 
+    def test_getSprintSamePriority(self):
+        # Make sure IBacklog.getSprint maximizes Points when all stories are the same Priority.
+        story1 = Story("1", 1, 1)
+        story2 = Story("2", 1, 1)
+        story3 = Story("3", 3, 1)
+        storylist = [story1, story2, story3]
+        datastore_mock = Mock()
+        datastore_mock.__class__ = IDatastore
+        datastore_mock.readStory.return_value = storylist
+        backlog = IBacklog(datastore_mock)
+        actual_storylist = backlog.getSprint(3)
+        self.assertIn(story3, actual_storylist)
+        self.assertNotIn(story1, actual_storylist)
+        self.assertNotIn(story2, actual_storylist)
+     
 
 if __name__ == '__main__':
     unittest.main()
